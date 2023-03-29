99481a1d0074248032fa868242c40c46a6595cac
Abhijit-shete007 <abhijit.shete1984@gmail.com>
2023-03-29 18:31:35 +0100

    Changed states to props

--- src/components/recipe/Recipe.jsx
+++ src/components/recipe/Recipe.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Recipe.scss";
 
-export default function recipe({ image, title, kitchen }) {
+export default function recipe(props) {
   return (
     <div className="recipe">
-      <img src={image} width="190px" alt="" />
-      <h2>{title}</h2>
-      <p>{kitchen}</p>
+      <img src={props.image} width="190px" alt="" />
+      <h2>{props.title}</h2>
+      <p>{props.kitchen}</p>
     </div>
   );
 }
